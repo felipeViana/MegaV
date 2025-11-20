7533403f58475e766b946177b3976e9926a1e83a
Felipe Viana <felipesousadeveloper@gmail.com>
2025-11-20 01:48:01 -0300

    added coyote timer and player animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,12 @@
 extends CharacterBody2D
 
+@onready var sprite = $AnimatedSprite2D
+@onready var coyoteTimer = $CoyoteTimer
+
 const SPEED = 300.0
 const JUMP_VELOCITY = -600.0
+
+var jumping
 
 var isGravityDown = 1
 
@@ -11,14 +16,18 @@ var shootTimer = 0
 const SHOOT_COOLDOWN = 0.25
 
 const BULLET_SPEED = 3000
-const BULLET_OFFSET = 100
+const BULLET_OFFSET = 50
 
 const PUSH_FORCE = 2000
 
 const Bullet = preload("res://scenes/bullet.tscn")
 const BulletType = preload("res://scripts/bullet.gd")
 
+var jumped = false
+
 func _process(delta: float) ->  void:
+	print(coyoteTimer.time_left)
+	
 	shootTimer -= delta
 	
 	if Input.is_action_just_pressed("shoot"):
@@ -32,16 +41,27 @@ func _process(delta: float) ->  void:
 		
 		shootTimer = SHOOT_COOLDOWN
 		
+		
+func canJump() -> bool:
+	return ((is_on_floor() or is_on_ceiling()) or not coyoteTimer.is_stopped()) and not jumped
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor() and not is_on_ceiling():
 		velocity += get_gravity() * delta * isGravityDown
+	else:
+		jumped = false
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or is_on_ceiling()):
+	if Input.is_action_just_pressed("ui_accept") and canJump():
+		jumped = true
 		velocity.y = JUMP_VELOCITY * isGravityDown
 		isGravityDown = isGravityDown * -1
+		if isGravityDown == 1:
+			sprite.flip_v = false
+		else:
+			sprite.flip_v = true
+		sprite.play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -49,10 +69,25 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction * SPEED
 		isPlayerGoingRight = direction
+		if isPlayerGoingRight == 1:
+			sprite.flip_h = false
+		else:
+			sprite.flip_h = true
+		sprite.play("run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		sprite.play("idle")
+		
+	if not is_on_ceiling() and not is_on_floor():
+		sprite.play("jump")
 
+	var wasOnFloorOrCeilling = is_on_ceiling() or is_on_floor()
+	
 	move_and_slide()
+	
+	#start coyote timer
+	if wasOnFloorOrCeilling and not is_on_ceiling() and not is_on_floor() and not jumped:
+		coyoteTimer.start()
 
 	# Check for collisions after moving
 	for i in range(get_slide_collision_count()):
